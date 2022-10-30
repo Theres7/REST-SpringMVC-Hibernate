67920c893d11c4f10a5337e444b5d4c3c29c0523
Theres Paul <113508191+Theres7@users.noreply.github.com>
2022-10-30 10:37:23 +0530

    Update UserController.java

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -7,19 +7,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController  // [since Spring 4, @RestController = @Controller + @ResponseBody. Postman -> to test REST methods]
+@RestController  
 public class UserController {
 
     @Autowired
     private UserService service;
-
+    
+    // Create User
     @PostMapping("/saveUser")
     public ResponseEntity<User> addUser(@RequestBody User user) {
         service.saveUser( user );
         var userResponseEntity = new ResponseEntity<User>( HttpStatus.CREATED );
         return userResponseEntity;
     }
-
+    
+    // Update User by Id
     @PutMapping("/updateUser/{userId}")
     public ResponseEntity<User> updateUser(@PathVariable("userId") int userId, @RequestBody User getUser) {
         User user = service.getUserById( userId );
@@ -34,12 +36,10 @@ public class UserController {
         return new ResponseEntity<>( HttpStatus.OK );
     }
 
-    // delete user
+    // Delete User by Id
     @DeleteMapping("/deleteUser/{userId}")
     public ResponseEntity<User> deleteUser(@PathVariable("userId") int userId, @ModelAttribute User user) {
         service.deleteUser( userId );
-//        return "redirect:/";
         return new ResponseEntity<>( user, HttpStatus.NO_CONTENT );
     }
-
 }
